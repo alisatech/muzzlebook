597b218f5f6621c4ea2d93f6d14ff7050f6a945f
AlisaBelyakova <6183112@gmail.com>
2018-07-24 11:46:10 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: ['babel-polyfill', './browser/index.js'],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
